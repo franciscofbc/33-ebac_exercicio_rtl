751f18e209f37eb15aed36e33e3df8926faa3892
Francisco Berti da Cruz <fbc991@live.com>
2023-04-24 22:54:24 -0300

    exercicio ok

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -3,8 +3,30 @@ import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+  it('Deve renderizar o componente corretamente', () => {
+    render(<PostComment />);
+    expect(screen.getByText('Comentar')).toBeInTheDocument();
+  });
+
+  it('deve inserir "linda" e "legal" nos comentarios', () => {
+    const { debug } = render(<PostComment />);
+    // render(<PostComment />);
+    fireEvent.change(screen.getByTestId('campo-comentario'), {
+      target: {
+        value: 'linda',
+      },
     });
+    fireEvent.click(screen.getByTestId('btn-cadastrar'));
+
+    fireEvent.change(screen.getByTestId('campo-comentario'), {
+      target: {
+        value: 'legal',
+      },
+    });
+    fireEvent.click(screen.getByTestId('btn-cadastrar'));
+
+    expect(screen.getByText('linda')).toBeInTheDocument();
+    expect(screen.getByText('legal')).toBeInTheDocument();
+    debug();
+  });
 });
